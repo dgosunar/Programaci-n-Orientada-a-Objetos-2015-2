4c8691892bfccc49607d34bc9d00b635517547da
dgosunar <dgosunar@unal.edu.co>
2015-09-18 22:39:08 -0500

    lo siento

--- Ahorcado/src/palabras.java
+++
@@ -1,8 +0,0 @@
-
-public class palabras {
-
-	 String  palabras [] = {"campamento", "murcielago", "juventud", "alianza", "compatible", "animales",
-		        "esperanza", "antiguedad" , "variable", "desubicado", "carpintero", "verdadero", "unicamente", 
-		        "universidad", "comunicacion", "analitico", "accesorio", "conservador", "ventana", "expontaneo"};
-		    
-	}
