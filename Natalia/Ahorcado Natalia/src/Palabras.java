b6e2f24f4a943b09385a6da37b85457f112f5aa8
Natalia Grijalba <ngrijalbah@unal.edu.co>
2015-09-19 21:07:05 -0500

    Ahorcado Natalia

---
+++ Natalia/Ahorcado Natalia/src/Palabras.java
@@ -0,0 +1,10 @@
+package Ahorcado Natalia;
+
+
+public class Palabras {
+
+	 String  palabras [] = {"campamento", "murcielago", "juventud", "alianza", "compatible", "animales",
+		        "esperanza", "antiguedad" , "variable", "desubicado", "carpintero", "verdadero", "unicamente", 
+		        "universidad", "comunicacion", "analitico", "accesorio", "conservador", "ventana", "expontaneo"};
+		    
+	}
